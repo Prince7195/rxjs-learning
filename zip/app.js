513246a1035196a330b177835a06982e1965da96
Vijay Deepak <vijaydeepak7195@outlook.com>
2017-10-14 11:45:03 +0530

    added zip app.js

---
+++ zip/app.js
@@ -0,0 +1,14 @@
+const sourceOne = Rx.Observable.of('Hello');
+const sourceTwo = Rx.Observable.of('World!');
+const sourceThree = Rx.Observable.of('Goodbye');
+const sourceFour = Rx.Observable.of('World!');
+//wait until all observables have emitted a value then emit all as an array
+const example = Rx.Observable
+  .zip(
+    sourceOne,
+    sourceTwo.delay(1000),
+    sourceThree.delay(2000),
+    sourceFour.delay(3000)
+  );
+//output: ["Hello", "World!", "Goodbye", "World!"]
+const subscribe = example.subscribe(val => console.log(val));
